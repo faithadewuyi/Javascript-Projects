a52561bcba23ea9598631d96458e8eabe415d7d0
faithadewuyi <iyanuleke09@gmail.com>
2024-01-20 16:25:18 +0100

    feat: Calculate age feature added

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,15 +1,33 @@
-const btnEl= document.getElementById("btn")
-const birthdayEl= document.getElementById("birthday")
+const btnElement= document.getElementById("btn")
+const birthdayEl= document.getElementById("birthday");
+const resultEl = document.getElementById('result')
 function calAge() {
     const dobValue= birthdayEl.value;
     if(dobValue===""){
     alert("Please enter your date of birth")
 
     }
+    else{
+        let age = getAge(dobValue);
+        resultEl.innerText = `Your age is ${age} ${age>1 ? "years" : "year"} old`;
+        
+       
+    }
+}
+function getAge(dobValue){
+    const currentDate = new Date();
+    const birthdayDate = new Date(dobValue);
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear(); 
+    // To calculate the month of birth
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+    if(month<0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate())){
+        age--
+    }
+   return(age)         
 }
 
-
-btnEl.addEventListener("click", calAge)
+btnElement.addEventListener("click", calAge)
 
 
   
