7c264202bbe71d1a53ea31a65687c3ea0290eeff
faithadewuyi <iyanuleke09@gmail.com>
2024-03-01 22:46:43 +0100

    updated

--- Counter App/script.js
+++ Counter App/script.js
@@ -1,8 +1,19 @@
 let count = 0;
 let countEl = document.getElementById("count-el")
+let saveEl= document.getElementById("save-el")
 
 function increment(){
    count++
-  countEl.innerText = count
+  countEl.textContent = count
+  
+}
+function save(){
+  let countStr = count + " - "
+  
+  saveEl.textContent += countStr
+
+  countEl.textContent = 0
+
+  count = 0
   
 }
