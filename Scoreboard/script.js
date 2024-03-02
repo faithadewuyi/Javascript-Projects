b1821cdf1af8b5597c5665d353d2358a33d32c99
faithadewuyi <iyanuleke09@gmail.com>
2024-03-02 22:01:34 +0100

    Score board refactored

---
+++ Scoreboard/script.js
@@ -0,0 +1,41 @@
+let homeScoreEl = document.getElementById("home-score")
+let homeScore = 0;
+function plus_one(){
+ homeScore++
+ 
+   homeScoreEl.textContent = homeScore
+
+}
+function plus_two(){
+  homeScore += 2
+  
+    homeScoreEl.textContent = homeScore
+ 
+ }
+ function plus_three(){
+  homeScore += 3
+  
+    homeScoreEl.textContent = homeScore
+ 
+ }
+
+ let guestScoreEl = document.getElementById("guest-score")
+let guestScore = 0;
+function plus_one(){
+ guestScore++
+ 
+   guestScoreEl.textContent = guestScore
+
+}
+function plus_two(){
+  guestScore += 2
+  
+    guestScoreEl.textContent = guestScore
+ 
+ }
+ function plus_three(){
+  guestScore += 3
+  
+    guestScoreEl.textContent = guestScore
+ 
+ }
