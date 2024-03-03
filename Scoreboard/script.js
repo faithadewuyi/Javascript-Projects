68d861732c105614c78537d96338625b7fdeafcc
faithadewuyi <iyanuleke09@gmail.com>
2024-03-03 18:11:16 +0100

    Feat: Highlight Leader's Score added

--- Scoreboard/script.js
+++ Scoreboard/script.js
@@ -1,22 +1,29 @@
 // Script for Home Score Buttons
 let homeScoreEl = document.getElementById("home-score")
 let homeScore = 0;
+
+function updateScores() {
+  document.getElementById('home-score').textContent = homeScore;
+  document.getElementById('guest-score').textContent = guestScore;
+  highLightLeader();
+}
+
 function plus_one(){
  homeScore++
  
-   homeScoreEl.textContent = homeScore
+ updateScores();
 
 }
 function plus_two(){
   homeScore += 2
   
-    homeScoreEl.textContent = homeScore
+  updateScores();
  
  }
  function plus_three(){
   homeScore += 3
   
-    homeScoreEl.textContent = homeScore
+  updateScores();
  
  }
 // Script for Guest Score Buttons
@@ -25,18 +32,36 @@ let guestScore = 0;
 function gplus_one(){
  guestScore++
  
-   guestScoreEl.textContent = guestScore
+ updateScores();
 
 }
 function gplus_two(){
   guestScore += 2
   
-    guestScoreEl.textContent = guestScore
+  updateScores();
  
  }
  function gplus_three(){
   guestScore += 3
   
-    guestScoreEl.textContent = guestScore
+  updateScores();
  
  }
+
+//  Function to highlight the leader
+
+ function highLightLeader(){
+  document.getElementById("home-score").classList.remove('leader');
+  document.getElementById("guest-score").classList.remove('leader');
+
+  if (homeScore > guestScore){
+    document.getElementById('home-score').classList.add('leader');
+  } else if (guestScore > homeScore) {
+    document.getElementById('guest-score').classList.add('leader');
+  }
+ }
+updateScores();
+ 
+
+
+ 
