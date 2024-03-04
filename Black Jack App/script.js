30afa482aef1055e440e9bf4ccdb54edeb128291
faithadewuyi <iyanuleke09@gmail.com>
2024-03-04 14:14:51 +0100

    BlackJack styled

--- Black Jack App/script.js
+++ Black Jack App/script.js
@@ -1,16 +1,34 @@
 let firstCard = 10;
 let secondCard = 7;
 
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard;
 
-if ( sum <= 20){
-  console.log ("Do you want to draw a new card? 🙂")
+let hasBlackJack = false;
+
+let outOfGame = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+function startGame(){
+  sumEl.textContent = "Sum:" + sum
+  if ( sum <= 20){
+    message= "Do you want to draw a new card? 🙂"
+
+  }
+  
+  else if(sum === 21)
+  {
+    message = "You've got Blackjack! 🥳"
+    hasBlackJack = true
+  } 
+  else{
+    message = "You're out of the game! 😭"
+  
+    outOfGame = false
+  }
+
+  messageEl.textContent = message
+  console.log(message)
 }
 
-else if(sum === 21)
-{
-  console.log("Wohoo! You've got Blackjack! 🥳")
-} 
-else{
-  console.log("You're out of the game! 😭")
-}
+
