3f552afd1462933478b6451e042841bbd058a9d8
faithadewuyi <iyanuleke09@gmail.com>
2024-03-21 12:29:55 +0100

    Most annoying cookie app is completed

--- Consent App/script.js
+++ Consent App/script.js
@@ -2,7 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
-const declineBtn = document.getElementById("decline-btn")
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -10,18 +11,18 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
-})
-declineBtn.addEventListener("mouseenter", function(){
+}) 
 
-  
-})
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
+}) 
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
     
     const consentFormData = new FormData(consentForm)
     const fullName = consentFormData.get('fullName')
-   
+    
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -42,8 +43,7 @@ consentForm.addEventListener('submit', function(e){
             <img src="images/pirate.gif">
         </div>
     `
-modalCloseBtn.disabled = false
-
+    modalCloseBtn.disabled = false
     }, 3000)
   
 }) 
